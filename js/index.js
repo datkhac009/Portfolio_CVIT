898a21e21f3bba0d418e41446c7813403d16523c
Đạt Nguyễn khắc <101082932+datkhac009@users.noreply.github.com>
2023-02-08 11:48:55 +0700

    update
    update scrollintoview

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-
+$('.owl-carousel').owlCarousel({
+    items:3,
+    loop: true,
+    lazyLoad:true,
+    autoplay: true,
+    animateOut: 'fadeOut',
+    margin:50,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+  const links = document.querySelectorAll(".scroll_to");
+  console.log(links);
+  links.forEach(items => {
+    items.addEventListener("click",()=>{
+      const el = document.getElementById(items.getAttribute("data-link"));
+      el.scrollIntoView({
+        behavior:"smooth",
+        block:"center"
+      })
+    })
+  });
